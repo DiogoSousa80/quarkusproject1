fbb8a13d7b1ef6df6a5e6a50a3d3b45ac7a02605
Diogo Sousa <sousadiogo453@gmail.com>
2023-04-26 22:46:48 -0300

    Criando a classe Customer no pacoete entity

---
+++ src/main/java/org/acme/entity/Customer.java
@@ -0,0 +1,13 @@
+package org.acme.entity;
+
+public class Customer {
+
+    private Long id;
+    private String nome;
+    private String lastName;
+    private Integer age;
+    private String email;
+
+
+
+}
